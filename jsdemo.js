0c9e9fa4925b9137d173073b5c127c5a80b61596
hehe345lol <oofergang336@gmail.com>
2026-06-25 15:48:23 -0400

    Add variables and log statements to jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file"); 
+var name = "khaleb";
+let school = "CATO";
+var fun_fact ="One ranked game brought me from rank 4409 on bardock to rank 23."
+
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Fun Fact: ${fun_fact}`);
