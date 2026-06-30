7089e4e69a5c295eb7ee64278501adf75bb99b09
hehe345lol <oofergang336@gmail.com>
2026-06-30 15:52:35 -0400

    Add HTML updates for displaying user information

--- jsdemo.js
+++ jsdemo.js
@@ -6,3 +6,7 @@ var fun_fact ="One ranked game brought me from rank 4409 on bardock to rank 23."
 console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
 console.log(`Fun Fact: ${fun_fact}`);
+
+document.getElementById("name").innerHTML = "<b>Name:</b><i>" + name + "</i>";
+document.getElementById("school").innerHTML = "<b>school:</b><i>" + school + "</i>";
+document.getElementById("fun_fact").innerHTML = "<b>fun fact:</b><i>" + fun_fact + "</i>";
