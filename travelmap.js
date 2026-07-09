6fbbf162b7014fe4fb00767ebd2c892ec6d87fd4
hehe345lol <oofergang336@gmail.com>
2026-07-09 15:30:13 -0400

    Fix typo in coordinates variable name

--- travelmap.js
+++ travelmap.js
@@ -5,4 +5,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-L.marker(coordninates, {"title": "hello from Japan!"}).bindPopup("<p>hello from Japan!</p>").addTo(map);
+L.marker(coordinates, {"title": "hello from Japan!"}).bindPopup("<p>hello from Japan!</p>").addTo(map);
