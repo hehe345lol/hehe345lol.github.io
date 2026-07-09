4acb579a1b5fb16c5fbca664e507086a380a2cad
hehe345lol <oofergang336@gmail.com>
2026-07-09 15:55:32 -0400

    Add second YouTube iframe to popup code
    Updated the popup code to include two YouTube iframes.

--- travelmap.js
+++ travelmap.js
@@ -2,7 +2,7 @@
 var coordinates = [35.6762, 139.6503];
 var popupcode = `<center>
 <h3>Hello from Japan!</h3>
-<iframe width="200" height="100" src="https://www.youtube.com/embed/IuTDuvYr7f0?si=SUQg8JXxj8nr-YG_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="300" height="200" src="https://www.youtube.com/embed/IuTDuvYr7f0?si=SUQg8JXxj8nr-YG_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </center`;
 var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
